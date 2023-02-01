f2db82cdb60bf14d74f56ef2a9e5cb20ab2a0893
rohitbisht01 <rb9519208@gmail.com>
2023-02-01 19:31:30 +0530

    OAuth integration

---
+++ src/components/GoogleAuth.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "390144549545-9a8i1s6s5jmblug52bnprrq845goqmup.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.auth.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
+  render() {
+    return <div>sing in</div>;
+  }
+}
+
+export default GoogleAuth;
