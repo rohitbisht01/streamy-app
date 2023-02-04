fddbca7713cf6b398d217fc44032d6a781fca8a8
rohitbisht01 <rb9519208@gmail.com>
2023-02-04 23:08:38 +0530

    added redux devtool extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 
 import App from "./App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
